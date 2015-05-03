070df462c4195bec56b3aa1a106173fcde8408ef
William Harris <Williamlharris21@gmail.com>
2015-05-03 15:24:51 -0400

    Upload

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,8 @@
+CarrierWave.configure do |config|
+  config.dropbox_app_key ="xjub09xyuoh1t5h"
+  config.dropbox_app_secret = "x2syv04yjoaxrcx" 
+  config.dropbox_access_token = "9ryomssnuj9oey8n "
+  config.dropbox_access_token_secret = "bv3wr8q9y8f9t4n"
+  config.dropbox_user_id = "364560768" 
+  config.dropbox_access_type = "app_folder"
+end 
